1ceb794e6cfecea392bfb8126a9fcbada9a52dad
Denis Vespasiano <43896714+DedeCefet@users.noreply.github.com>
2020-07-07 09:27:00 -0300

    inicialização de servidor
    Inicialização do servidor em Node.js

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express = require("express");
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.listen(3333);
