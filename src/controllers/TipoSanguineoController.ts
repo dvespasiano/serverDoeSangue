cf884fbb448fb5166170b7eb2d58b7519ae8e900
Denis Vespasiano <43896714+DedeCefet@users.noreply.github.com>
2020-07-07 11:42:49 -0300

    atualização de dependencias do banco de dados
    Atualização de dependncias do banco de dados

--- src/controllers/TipoSanguineoController.ts
+++ src/controllers/TipoSanguineoController.ts
@@ -3,6 +3,18 @@ import knex from '../database/connection'
 import { Request, Response, response } from 'express'
 
 class TipoSanguineoController {
+    async post(request: Request, respons: Response) {
+        const {
+            nmTipoSanguineo,
+        } = request.body;
+        const trx = await knex.transaction();
+        const tpSanguineo = {
+            nmTipoSanguineo,
+        }
+        await trx('tipoSanguineo').insert(tpSanguineo)
+        await trx.commit();
+        return response.json(tpSanguineo);
+    }
 }
 
 export default TipoSanguineoController;
