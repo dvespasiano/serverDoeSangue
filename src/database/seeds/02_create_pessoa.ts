c15bebf6d3706b58990e087b27f651e4a0903d4b
DedeCefet <43896714+DedeCefet@users.noreply.github.com>
2020-07-09 20:36:42 -0300

    att
    att

---
+++ src/database/seeds/02_create_pessoa.ts
@@ -0,0 +1,14 @@
+import Knex = require('knex');
+
+export async function seed(knex: Knex) {
+    await knex('estoque').insert([
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 1 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 2 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 3 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 4 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 5 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 6 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 7 },
+        { qtd: '50', qtdMinima: '30', tipoSanguineo_id: 8 },
+    ]);
+}
