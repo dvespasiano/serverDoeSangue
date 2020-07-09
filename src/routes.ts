48cd478a27ac1af07577f47d6ee5f8cb8dd61da8
DedeCefet <43896714+DedeCefet@users.noreply.github.com>
2020-07-09 19:57:17 -0300

    att
    att

--- src/routes.ts
+++ src/routes.ts
@@ -2,21 +2,16 @@ import express = require('express');
 import knex from './database/connection';
 import EstoqueController from './controllers/EstoqueController';
 import PessoaController from './controllers/PessoaController';
-import RoleController from './controllers/RoleController';
 import TipoSanguineoController from './controllers/TipoSanguineoController';
 
 const routes = express.Router()
 const estoqueController = new EstoqueController()
 const pessoaController = new PessoaController()
-const roleController = new RoleController()
 const tipoSanguineoController = new TipoSanguineoController()
 
 //tipoSanguineo
 // routes.post('/tiposanguineo', tipoSanguineoController.post)
 routes.get('/tiposanguineo', tipoSanguineoController.getAll)
-
-//roles
-routes.get('/roles', roleController.getAll)
 
 //Estoque
 routes.get('/estoque', estoqueController.getAll)
